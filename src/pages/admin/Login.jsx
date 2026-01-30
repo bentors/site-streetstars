a5bda266be7f04622a8ca3ed3f067058ef464502
bentors <bento.rangel05@gmail.com>
2026-01-30 02:27:05 -0300

    Correção de 'erro' no import da Logo

--- src/pages/admin/Login.jsx
+++ src/pages/admin/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '../../services/firebase'
-import Logo from '../../components/ui/logo'
+import Logo from '../../components/ui/Logo'
 
 export default function Login() {
   const [email, setEmail] = useState('')
